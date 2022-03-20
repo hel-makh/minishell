3ed566615d90f6751801315b331bccb2c3eb8e90
Hamza El makhroubi <hel-makh@e2r8p3.1337.ma>
2022-03-20 15:37:29 +0100

    fixed quotes error

--- srcs/ft_tokenization.c
+++ srcs/ft_tokenization.c
@@ -6,7 +6,7 @@
 /*   By: hel-makh <hel-makh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 11:39:48 by hel-makh          #+#    #+#             */
-/*   Updated: 2022/03/20 14:36:34 by hel-makh         ###   ########.fr       */
+/*   Updated: 2022/03/20 15:37:15 by hel-makh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,50 @@ static char	*ft_str2chr(const char *s, const char *s2)
 		i ++;
 	}
 	return (NULL);
+}
+
+static size_t	ft_end_of_token(char *cmd, size_t size)
+{
+	if (ft_str2chr(cmd, "<>&|"))
+	{
+		if (size == 1)
+			return (size + 1);
+		return (size - 1);
+	}
+	else if (ft_strchr(" ()<>|*", cmd[0]) || ((cmd[0] == '\'' || cmd[0] == '"')
+			&& (!cmd[1] || (cmd[1] && !ft_strchr(&cmd[1], cmd[0])))))
+	{
+		if (size == 1)
+			return (size);
+		return (size - 1);
+	}
+	return (0);
+}
+
+static size_t	ft_get_token_size(t_vars *vars, int i)
+{
+	char	*cmd;
+	char	quote;
+	size_t	size;
+
+	cmd = vars->cmdline;
+	quote = 0;
+	size = 0;
+	while (cmd[++i])
+	{
+		size ++;
+		if (!quote && (cmd[i] == '\'' || cmd[i] == '"'))
+			quote = cmd[i];
+		else if (quote)
+		{
+			if (cmd[i] == quote)
+				quote = 0;
+			continue ;
+		}
+		if (ft_end_of_token(&cmd[i], size))
+			return (ft_end_of_token(&cmd[i], size));
+	}
+	return (size);
 }
 
 static int	ft_get_token_type(char *token_data)
@@ -55,49 +99,6 @@ static int	ft_get_token_type(char *token_data)
 	else if (!ft_strcmp(token_data, "*"))
 		return (WILDCARD);
 	return (WORD);
-}
-
-static size_t	ft_get_token_size(t_vars *vars, int i)
-{
-	char	*cmd;
-	char	quote;
-	size_t	size;
-
-	cmd = vars->cmdline;
-	quote = 0;
-	size = 0;
-	while (cmd[++i])
-	{
-		size ++;
-		if (!quote && (cmd[i] == '\'' || cmd[i] == '"')
-			&& cmd[i + 1] && !ft_strchr(&cmd[i + 1], cmd[i]))
-		{
-			if (size == 1)
-				return (size);
-			return (size - 1);
-		}
-		if (!quote && (cmd[i] == '\'' || cmd[i] == '"'))
-			quote = cmd[i];
-		else if (quote)
-		{
-			if (cmd[i] == quote)
-				quote = 0;
-			continue ;
-		}
-		if (ft_str2chr(&cmd[i], "<>&|"))
-		{
-			if (size == 1)
-				return (size + 1);
-			return (size - 1);
-		}
-		else if (ft_strchr(" ()<>|*", cmd[i]))
-		{
-			if (size == 1)
-				return (size);
-			return (size - 1);
-		}
-	}
-	return (size);
 }
 
 void	ft_tokenization(t_vars *vars)
