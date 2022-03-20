831aaa47717338dd4ab4055d0538efb96e5c7422
Hamza El makhroubi <hel-makh@e2r8p3.1337.ma>
2022-03-20 20:04:51 +0100

    added shlbl

--- srcs/ft_cmd_lstnew.c
+++ srcs/ft_cmd_lstnew.c
@@ -6,13 +6,13 @@
 /*   By: hel-makh <hel-makh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 11:19:55 by hel-makh          #+#    #+#             */
-/*   Updated: 2022/03/20 18:10:35 by hel-makh         ###   ########.fr       */
+/*   Updated: 2022/03/20 18:56:12 by hel-makh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-t_cmd	*ft_cmd_lstnew(char **cmd, int type, t_list *redirect)
+t_cmd	*ft_cmd_lstnew(char **cmd, int type, int shlvl, t_list *redirect)
 {
 	t_cmd	*element;
 
@@ -21,6 +21,7 @@ t_cmd	*ft_cmd_lstnew(char **cmd, int type, t_list *redirect)
 		return (0);
 	element->cmd = cmd;
 	element->type = type;
+	element->shlvl = shlvl;
 	element->redirect = redirect;
 	element->next = NULL;
 	return (element);
