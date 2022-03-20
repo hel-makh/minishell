9bc66416cbc47a630eb35645c44142c3ece230bc
Hamza El makhroubi <hel-makh@e2r8p3.1337.ma>
2022-03-20 20:04:34 +0100

    fixed error

--- srcs/ft_cmd_lstclear.c
+++ srcs/ft_cmd_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: hel-makh <hel-makh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 18:03:33 by hel-makh          #+#    #+#             */
-/*   Updated: 2022/03/20 18:08:57 by hel-makh         ###   ########.fr       */
+/*   Updated: 2022/03/20 19:26:18 by hel-makh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	ft_cmd_lstclear(t_cmd **lst)
 		holder = *lst;
 		*lst = (*lst)->next;
 		holder->cmd = ft_free_2d(holder->cmd);
-		ft_lstclear(holder->redirect);
+		ft_lstclear(&holder->redirect);
 		holder = ft_free(holder);
 	}
 	*lst = NULL;
