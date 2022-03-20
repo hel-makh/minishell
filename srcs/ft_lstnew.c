74c97b35c67bb6f204ffb5e8faa886a203f80964
Hamza El makhroubi <hel-makh@e2r8p3.1337.ma>
2022-03-20 16:08:44 +0100

    fixed typo

--- srcs/ft_lstnew.c
+++ srcs/ft_lstnew.c
@@ -6,13 +6,13 @@
 /*   By: hel-makh <hel-makh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 11:19:55 by hel-makh          #+#    #+#             */
-/*   Updated: 2022/03/20 16:00:06 by hel-makh         ###   ########.fr       */
+/*   Updated: 2022/03/20 16:08:02 by hel-makh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-t_list	*ft_token_lstnew(char *content, int type)
+t_list	*ft_lstnew(char *content, int type)
 {
 	t_list	*element;
 
