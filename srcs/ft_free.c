ac5625dfc518f2f28f60df3164aa4b0eb034de94
Hamza El makhroubi <hel-makh@e2r7p6.1337.ma>
2022-03-21 13:13:20 +0100

    added protection

--- srcs/ft_free.c
+++ srcs/ft_free.c
@@ -6,7 +6,7 @@
 /*   By: hel-makh <hel-makh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 16:58:07 by hel-makh          #+#    #+#             */
-/*   Updated: 2022/03/20 14:56:20 by hel-makh         ###   ########.fr       */
+/*   Updated: 2022/03/21 12:58:41 by hel-makh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ void	*ft_free_2d(char **ptr)
 {
 	int	i;
 
+	if (!ptr)
+		return (NULL);
 	i = 0;
 	while (ptr[i])
 		free(ptr[i++]);
@@ -34,6 +36,8 @@ void	*ft_free_3d(char ***ptr)
 {
 	int	i;
 
+	if (!ptr)
+		return (NULL);
 	i = 0;
 	while (ptr[i])
 		ft_free_2d(ptr[i++]);
