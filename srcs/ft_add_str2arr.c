b4db8a72e24d1f0543671a7863192eca9149b7e1
Hamza El makhroubi <hel-makh@e2r8p3.1337.ma>
2022-03-22 17:25:50 +0100

    fixed null array error

--- srcs/ft_add_str2arr.c
+++ srcs/ft_add_str2arr.c
@@ -6,7 +6,7 @@
 /*   By: hel-makh <hel-makh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/20 19:50:13 by hel-makh          #+#    #+#             */
-/*   Updated: 2022/03/20 19:50:28 by hel-makh         ###   ########.fr       */
+/*   Updated: 2022/03/22 16:49:31 by hel-makh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@ char	**ft_add_str2arr(char **array, char *str)
 	size_t	array_len;
 	size_t	i;
 
-	array_len = ft_arrlen(array);
+	array_len = 0;
+	if (array)
+		array_len = ft_arrlen(array);
 	new_array = (char **) ft_calloc(array_len + 2, sizeof(char *));
 	if (!new_array)
 		return (NULL);
