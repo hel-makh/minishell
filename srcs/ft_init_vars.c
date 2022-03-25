b68ee575bb10d642a5533bbf2390e4b63b917cbf
Hamza El makhroubi <hel-makh@e2r8p3.1337.ma>
2022-03-25 12:23:22 +0100

    added protection

--- srcs/ft_init_vars.c
+++ srcs/ft_init_vars.c
@@ -6,7 +6,7 @@
 /*   By: hel-makh <hel-makh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 17:36:04 by hel-makh          #+#    #+#             */
-/*   Updated: 2022/03/24 23:48:51 by hel-makh         ###   ########.fr       */
+/*   Updated: 2022/03/25 12:23:03 by hel-makh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ int	ft_init_vars(t_vars *vars, char *envp[])
 	if (!t_envp)
 		return (0);
 	vars->envp = ft_env_lstnew(t_envp);
+	if (!vars->envp)
+		return (ft_free_2d(t_envp), 0);
 	vars->last_cmdline = NULL;
 	vars->tokens = NULL;
 	return (1);
