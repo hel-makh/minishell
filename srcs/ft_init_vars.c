14c462a1a65414fd91ad343fc7760f9adcdbfdb2
Hamza El makhroubi <hel-makh@e2r8p3.1337.ma>
2022-03-25 12:37:03 +0100

    modification

--- srcs/ft_init_vars.c
+++ srcs/ft_init_vars.c
@@ -6,7 +6,7 @@
 /*   By: hel-makh <hel-makh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 17:36:04 by hel-makh          #+#    #+#             */
-/*   Updated: 2022/03/25 12:23:03 by hel-makh         ###   ########.fr       */
+/*   Updated: 2022/03/25 12:24:33 by hel-makh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,15 @@ int	ft_init_vars(t_vars *vars, char *envp[])
 {
 	char	**t_envp;
 
+	vars->cmdline = NULL;
+	vars->last_cmdline = NULL;
+	vars->tokens = NULL;
+	vars->cmds = NULL;
 	t_envp = ft_arrdup(envp);
 	if (!t_envp)
 		return (0);
 	vars->envp = ft_env_lstnew(t_envp);
 	if (!vars->envp)
 		return (ft_free_2d(t_envp), 0);
-	vars->last_cmdline = NULL;
-	vars->tokens = NULL;
 	return (1);
 }
