01435af795b726fd7a2ad6ed2f1e1247d76b80fe
Hamza El makhroubi <hel-makh@e2r7p4.1337.ma>
2022-03-27 23:30:14 +0000

    fixed env cmd

--- srcs/exec_env.c
+++ srcs/exec_env.c
@@ -6,7 +6,7 @@
 /*   By: hel-makh <hel-makh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/26 21:33:18 by hel-makh          #+#    #+#             */
-/*   Updated: 2022/03/26 21:35:00 by hel-makh         ###   ########.fr       */
+/*   Updated: 2022/03/27 23:29:55 by hel-makh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ int	builtin_env(char **envp)
 	i = 0;
 	while (envp[i])
 	{
-		ft_putendl_fd(envp[i], STDOUT_FILENO);
+		if (ft_strchr(envp[i], '='))
+			ft_putendl_fd(envp[i], STDOUT_FILENO);
 		i ++;
 	}
 	return (1);
