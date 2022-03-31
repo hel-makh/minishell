322bbb17cbc9de61007b192e18d990f776c4c193
Hamza El makhroubi <hel-makh@e2r8p3.1337.ma>
2022-03-31 00:11:19 +0000

    changed exit status

--- srcs/exec_pwd.c
+++ srcs/exec_pwd.c
@@ -6,7 +6,7 @@
 /*   By: hel-makh <hel-makh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 17:16:11 by ybensell          #+#    #+#             */
-/*   Updated: 2022/03/28 17:13:28 by hel-makh         ###   ########.fr       */
+/*   Updated: 2022/03/31 00:07:16 by hel-makh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	builtin_pwd(void)
 	if (!getcwd(buff, 4096))
 	{
 		perror("minishell");
-		return (1);
+		return (EXIT_FAILURE);
 	}
 	ft_putendl_fd(buff, STDOUT_FILENO);
 	return (EXIT_SUCCESS);
