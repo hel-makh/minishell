df13313375cd40529e50f0d5c092a1696652c3a5
Hamza El makhroubi <hel-makh@e2r8p5.1337.ma>
2022-04-05 13:59:27 +0000

    added cd -

--- srcs/exec_cd.c
+++ srcs/exec_cd.c
@@ -6,7 +6,7 @@
 /*   By: hel-makh <hel-makh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 16:52:39 by ybensell          #+#    #+#             */
-/*   Updated: 2022/03/28 17:21:31 by hel-makh         ###   ########.fr       */
+/*   Updated: 2022/04/05 13:57:35 by hel-makh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,25 +37,46 @@ static int	change_pwd_env(char *old_cwd, char ***envp)
 	return (EXIT_SUCCESS);
 }
 
-int	builtin_cd(char **cmd, char ***envp)
+static int	cd_oldpwd(char *old_cwd, char ***envp)
+{
+	char	*old_pwd;
+
+	old_pwd = ft_getenv("OLDPWD", *envp);
+	if (!old_pwd)
+		return (ft_putendl_fd("minishell: cd: OLDPWD not set", 2), 0);
+	if (chdir(old_pwd) == -1)
+		return (0);
+	if (!change_pwd_env(old_cwd, envp))
+		return (0);
+	return (1);
+}
+
+static int	cd_home(char *old_cwd, char ***envp)
 {
 	char	*home_path;
+
+	home_path = ft_getenv("HOME", *envp);
+	if (!home_path)
+		return (ft_putendl_fd("minishell: cd: HOME not set", 2), 0);
+	if (chdir(home_path) == -1)
+		return (0);
+	if (!change_pwd_env(old_cwd, envp))
+		return (0);
+	return (1);
+}
+
+int	builtin_cd(char **cmd, char ***envp)
+{
 	char	cwd[4096];
 
 	if (!getcwd(cwd, 4096))
 		return (EXIT_FAILURE);
-	if (!cmd[1])
-	{
-		home_path = ft_getenv("HOME", *envp);
-		if (!home_path)
-			return (ft_putendl_fd("minishell: cd: HOME not set", 2),
-				EXIT_FAILURE);
-		if (chdir(home_path) == -1)
-			return (EXIT_FAILURE);
-		return (change_pwd_env(cwd, envp));
-	}
+	if (!cmd[1] && !cd_home(cwd, envp))
+		return (EXIT_FAILURE);
 	if (!*cmd[1])
 		return (EXIT_SUCCESS);
+	if (!ft_strcmp(cmd[1], "-") && !cd_oldpwd(cwd, envp))
+		return (EXIT_FAILURE);
 	if (chdir(cmd[1]) == -1)
 	{
 		ft_putstr_fd("minishell: cd: ", 2);
