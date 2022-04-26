763f72248385fa0213facb17b990f4769cf017ab
Hamza El makhroubi <hel-makh@e2r7p13.1337.ma>
2022-04-26 01:58:45 +0000

    fixed non numeric arg error

--- srcs/exec_exit.c
+++ srcs/exec_exit.c
@@ -6,28 +6,30 @@
 /*   By: hel-makh <hel-makh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 10:11:20 by ybensell          #+#    #+#             */
-/*   Updated: 2022/03/30 17:59:25 by hel-makh         ###   ########.fr       */
+/*   Updated: 2022/04/26 01:54:21 by hel-makh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-static int	is_numb(const char *s)
+static int	is_numeric(const char *s)
 {
 	int	i;
+	int	is_numeric;
 
 	i = 0;
-	while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n'
-		|| s[i] == '\r' || s[i] == '\v' || s[i] == '\f')
+	is_numeric = 0;
+	while (s[i] && ft_isspace(s[i]))
 		i++;
 	if (s[i] == '-' || s[i] == '+')
 		i++;
-	while (ft_isdigit(s[i]))
+	while (s[i] && ft_isdigit(s[i++]))
+		is_numeric = 1;
+	while (s[i] && ft_isspace(s[i]))
 		i++;
-	while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n'
-		|| s[i] == '\r' || s[i] == '\v' || s[i] == '\f')
-		i++;
-	return (s[i] == '\0');
+	if (s[i] != '\0')
+		is_numeric = 0;
+	return (is_numeric);
 }
 
 static double	ft_atoi_exit(const char *str)
@@ -37,8 +39,7 @@ static double	ft_atoi_exit(const char *str)
 
 	sign = 1;
 	nbr = 0;
-	while (*str == ' ' || *str == '\t' || *str == '\n'
-		|| *str == '\v' || *str == '\r' || *str == '\f')
+	while (ft_isspace(*str))
 		str ++;
 	if (*str == '+' || *str == '-')
 	{
@@ -46,7 +47,7 @@ static double	ft_atoi_exit(const char *str)
 			sign *= -1;
 		str ++;
 	}
-	while (*str >= '0' && *str <= '9')
+	while (ft_isdigit(*str))
 	{
 		nbr = (nbr * 10) + (*str - '0');
 		str ++;
@@ -66,7 +67,7 @@ int	builtin_exit(char **cmd)
 	if (size == 2)
 	{
 		ret = ft_atoi_exit(cmd[1]);
-		if (!is_numb(cmd[1]) || ret > LONG_MAX || ret < LONG_MIN)
+		if (!is_numeric(cmd[1]) || ret > LONG_MAX || ret < LONG_MIN)
 		{
 			ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
 			ft_putstr_fd(cmd[1], STDERR_FILENO);
