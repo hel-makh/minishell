2a51e772a33d42a3b01029efeef5072894ee6c3a
Hamza El makhroubi <hel-makh@e2r8p13.1337.ma>
2022-04-28 23:28:39 +0000

    fixed args management

--- srcs/exec_exit.c
+++ srcs/exec_exit.c
@@ -6,7 +6,7 @@
 /*   By: hel-makh <hel-makh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 10:11:20 by ybensell          #+#    #+#             */
-/*   Updated: 2022/04/26 01:54:21 by hel-makh         ###   ########.fr       */
+/*   Updated: 2022/04/28 23:26:57 by hel-makh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,20 @@
 
 static int	is_numeric(const char *s)
 {
-	int	i;
 	int	is_numeric;
+	int	i;
 
-	i = 0;
 	is_numeric = 0;
+	i = 0;
 	while (s[i] && ft_isspace(s[i]))
 		i++;
 	if (s[i] == '-' || s[i] == '+')
 		i++;
-	while (s[i] && ft_isdigit(s[i++]))
+	while (s[i] && ft_isdigit(s[i]))
+	{
 		is_numeric = 1;
+		i ++;
+	}
 	while (s[i] && ft_isspace(s[i]))
 		i++;
 	if (s[i] != '\0')
@@ -57,29 +60,25 @@ static double	ft_atoi_exit(const char *str)
 
 int	builtin_exit(char **cmd)
 {
-	double		ret;
+	double		exit_status;
 	int			size;
 
 	ft_putendl_fd("exit", STDOUT_FILENO);
 	size = ft_arrlen(cmd);
 	if (size == 1)
 		exit(g_glob.exit_status);
-	if (size == 2)
+	exit_status = ft_atoi_exit(cmd[1]);
+	if (!is_numeric(cmd[1]) || exit_status > LONG_MAX || exit_status < LONG_MIN)
 	{
-		ret = ft_atoi_exit(cmd[1]);
-		if (!is_numeric(cmd[1]) || ret > LONG_MAX || ret < LONG_MIN)
-		{
-			ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
-			ft_putstr_fd(cmd[1], STDERR_FILENO);
-			ft_putendl_fd(": numeric argument required", STDERR_FILENO);
-			exit(255);
-		}
-		else
-			exit((unsigned char)ret);
+		ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
+		ft_putstr_fd(cmd[1], STDERR_FILENO);
+		ft_putendl_fd(": numeric argument required", STDERR_FILENO);
+		exit(255);
 	}
-	else
+	if (size > 2)
 	{
 		ft_putendl_fd("minishell: exit: too many arguments", STDERR_FILENO);
 		return (EXIT_FAILURE);
 	}
+	exit(exit_status);
 }
